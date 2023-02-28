742d7d1a2cf0370e2feeccc9a87eb41dbc7c0eeb
zcy-eternity <34476241@qq.com>
2023-02-28 15:35:00 +0800

    bug0

--- consumer/src/main/java/cn/itcast/mq/service/UserService.java
+++ consumer/src/main/java/cn/itcast/mq/service/UserService.java
@@ -3,6 +3,7 @@ package cn.itcast.mq.service;
 public class UserService {
     public void add(){
         int x = 10;
-        int y = 10/0;
+        int y = 10/x;
     }
+    public void fix(){}
 }
